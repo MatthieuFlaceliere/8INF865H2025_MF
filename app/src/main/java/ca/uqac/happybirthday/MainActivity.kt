f7dd36ac4c2cd8bd3810a5204446a75bb8b58fff
Matthieu Flce <flacelierematthieu@gmail.com>
2025-01-18 16:12:59 -0500

    Step 4 image

--- app/src/main/java/ca/uqac/happybirthday/MainActivity.kt
+++ app/src/main/java/ca/uqac/happybirthday/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -32,10 +33,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
+                    GreetingImage(
                         "Happy Birthday Sam!",
                         "From Emma",
-                        Modifier.padding(8.dp)
                     )
                 }
             }
@@ -46,11 +46,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty)
-    Image(
-        painter = image,
-        contentDescription = null,
-        modifier = modifier
-    )
+    Box(
+        modifier = modifier,
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+
 }
 
 @Composable
